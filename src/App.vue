6a2d2a78f95754bdf3d37cb3c301c1f4a932e325
judyta wlodarska <jdtwlodar@gmail.com>
2020-06-16 19:48:04 +0200

    components add

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <div class="container-fluid px-0">
+        <div class="d-flex flex-column flex-md-row">
+      <div class="order-2 order-md-1 col-md-6 px-0">
+        <Content />
+      </div>
+      <div class="order-2 order-md-1 col-md-6 px-0"></div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Content from "./components/Content.vue";
+
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Content,
+
+  },
+};
 </script>
 
 <style lang="scss">
-@import 'node_modules/bootstrap/scss/bootstrap';
+@import "node_modules/bootstrap/scss/bootstrap";
 .app {
   &__logo {
     width: 2rem;
@@ -28,6 +37,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
 }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">
+        <img class="app__logo" src="../assets/images/logo.png" alt />
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#">Link</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <QA />
+  </div>
+</template>
+
+<script>
+import QA from "./QA.vue";
+
+
+export default {
+  name: "Content",
+  data() {
+    return {
+      title: "Jestem menu",
+    };
+  },
+  components: {
+    QA
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
